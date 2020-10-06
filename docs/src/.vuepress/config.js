2f4181937d4de691d86a78442c30d964f8610516
RuijianZha <1323318674@qq.com>
2020-10-06 23:41:55 +0800

    Update config.js

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Algorithm && Life Haker Tips',
+  title: 'Algorithm && Life Hacker Tips',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
