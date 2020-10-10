114458dbcb005c1f39b5344c466a2d0ac55883f7
RuijianZha <1323318674@qq.com>
2020-10-10 16:55:26 +0800

    Update config.js

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -28,6 +28,7 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
+    subSidebar: 'auto',
     authorAvatar: '/tou_xiang.png',
     type: 'blog',
     author: 'Ruijian_Zha',
